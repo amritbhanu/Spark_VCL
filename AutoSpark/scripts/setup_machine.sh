7b1fbd2253bcec3979f73b3692febba0e00c46ca
aagrawa8 <aagrawa8@ncsu.edu>
2016-02-23 13:42:05 -0500

    save

--- AutoSpark/scripts/setup_machine.sh
+++ AutoSpark/scripts/setup_machine.sh
@@ -1,16 +1,4 @@
 # Updates the instances
-sudo apt-get update -y
-
-# Install pip
-sudo apt-get install python-pip -y
-
-# Setting up npm
-sudo apt-get install npm -y
-
-# Setting up node js
-sudo apt-get install nodejs-legacy -y
-sudo ln -s /usr/bin/nodejs /usr/sbin/node
-
 # Install ansible
 sudo apt-get install software-properties-common -y
 sudo apt-add-repository ppa:ansible/ansible -y
